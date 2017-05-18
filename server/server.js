d925b0fb0efa551f361ab14aa97568baa00a429a
tundergod <tundergod1882@gmail.com>
2017-05-18 12:59:29 +0000

    hello

--- server/server.js
+++ server/server.js
@@ -6,12 +6,13 @@
 var http = require('http')
 var express = require('express')
 var calcDistance = require('./calcDistance')
-
+var bodyParser = require('body-parser')
 // create an express app
 var app = express()
 
 // view engine setup
 app.use(express.static(__dirname + '/webApplication'))
+app.use(bodyParser())
 
 // create a server
 var server = http.createServer(app)
@@ -22,20 +23,24 @@ app.use('/', function (request, response, next) {
   next()
 })
 
-app.get('/', function(request, response, next){
-	response.sendfile('/index.html')
-	next()
+app.get('/', function (request, response, next) {
+  response.sendfile('/index.html')
+  next()
 })
 
-//handle PUT request from each cup
+// handle PUT request from each cup
 app.put('/', function (request, response, next) {
   var cupData = request.body
 //  var cupID = request.params.id
   console.log('Request type : PUT')
-  console.log(cupData)
+  console.log(JSON.stringify(cupData))
+ // console.log(cupID)
+  console.log('Distance V1= ' + calcDistance.countDistanceV1(cupData.rssi))
+  console.log('Distance V2= ' + calcDistance.countDistanceV1(cupData.rssi))
+  console.log()
 })
 
-//listening to specific IP addr and port
+// listening to specific IP addr and port
 server.listen(3000, '127.0.0.1', function () {
   console.log('Server Start!')
   console.log('Listening port 3000......')
